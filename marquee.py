cebfb8db37856fd900ba6fc03d795a5354cff0ab
mcampos-quinn <mcq@berkeley.edu>
2017-12-11 17:25:08 -0800

    hey, it works

--- marquee.py
+++ marquee.py
@@ -4,28 +4,55 @@ import pyodbc
 import configparser
 import os
 
-marqueeDir = os.dirname(os.path.abspath(__file__))
-configPath = os.path.join(marqueeDir,'config','config.ini')
-config = configparser.SafeConfigParser()
-config.read(configPath)
-
-outputDir = os.path.join(marqueeDir,'output-files')
-
-def query():
+def query(host,user,pw):
 	try:
-		c = pyodbc.connect("DRIVER={FileMaker ODBC};DSN=marquee;SERVER=bampfa-pfm13.ist.1918.berkeley.edu;UID="+user+";PWD="+cred)
-		c.setdecoding(pyodbc.SQL_CHAR, encoding='utf-8')
-		c.setdecoding(pyodbc.SQL_WCHAR, encoding='utf-8')
-		c.setdecoding(pyodbc.SQL_WMETADATA, encoding='utf-8')
-		c.setencoding(encoding='utf-8')
+		c = pyodbc.connect("DRIVER={FileMaker ODBC};DATABASE=Marquee;SERVER="+host+";UID="+user+";PWD="+pw)
+		print(c)
+		# c.setdecoding(pyodbc.SQL_CHAR, encoding='utf-8')
+		# c.setdecoding(pyodbc.SQL_WCHAR, encoding='utf-8')
+		# c.setdecoding(pyodbc.SQL_WMETADATA, encoding='utf-8')
+		# c.setencoding(encoding='utf-8')
 		cursor= c.cursor()
 		
-		# SQL TO GET REQUIRED METADATA VALUES FROM FM
-		cursor.execute("""SELECT m_245a_CompleteTitle, AlternativeTitle, 
-			AccessionNumberPrefix, AccessionNumberDepositorNumber, 
-			AccessionNumberItemNumber, ProjectGroupTitle,
-			m_257a_Country, m_260c_ReleaseYear,
-			ct_DirectorsNames, Credits,
-			GeneralNotes, m_945z_GeneralConditionNotes
-			FROM CollectionItem WHERE AccessionNumberItemNumber = ?""",idNumber)	
+		# SQL TO GET REQUIRED VALUES FROM FM
+		cursor.execute("""SELECT field01,field02,field03,field04,
+			field05,field06,field07,field08
+			FROM marquee""")	
 		rows = cursor.fetchall()
+		for result in rows:
+			if result == None:
+				result = ''
+
+		currentValues = [x for y in rows for x in y]
+		return currentValues
+	except:
+		print("something failed in the FileMaker query")
+
+def write():
+	marqueeDir = os.path.dirname(os.path.abspath(__file__))
+	configPath = os.path.join(marqueeDir,'config','config.ini')
+	outputDir = os.path.join(marqueeDir,'output-files')
+	print(configPath)
+	print(outputDir)	
+	config = configparser.SafeConfigParser()
+	config.read_file(open(configPath,'r'))
+	user = config['FileMaker']['user']
+	pw = config['FileMaker']['pass']
+	host = config['FileMaker']['host']
+	# print(os.listdir(outputDir))
+	# print(query())
+	for textfile in os.listdir(outputDir):
+		textfilePath = os.path.join(outputDir,textfile)
+		if textfile.endswith(".txt"):
+			fileNumber = int(textfile[0])
+			fileIndex = fileNumber - 1
+			try: 
+				with open(textfilePath,'w+') as valuefile:
+					valuefile.write(query(host,user,pw)[fileIndex])
+			except:
+				print("something went wrong")
+				pass
+				print(valuefile)
+
+if __name__ == '__main__':
+	write()
