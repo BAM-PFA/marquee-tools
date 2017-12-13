bf0b39b2a3bfe09c263ce3130ca20cefa268c5c0
mcampos-quinn <mcq@berkeley.edu>
2017-12-12 16:26:32 -0800

    refactor for production filepaths and expected filenames

--- marquee.py
+++ marquee.py
@@ -4,9 +4,9 @@ import pyodbc
 import configparser
 import os
 
-def query(host,user,pw):
+def query(host,db,user,pw):
 	try:
-		c = pyodbc.connect("DRIVER={FileMaker ODBC};DATABASE=Marquee;SERVER="+host+";UID="+user+";PWD="+pw)
+		c = pyodbc.connect("DRIVER={FileMaker ODBC};DATABASE="+db+";SERVER="+host+";UID="+user+";PWD="+pw)
 		print(c)
 		# c.setdecoding(pyodbc.SQL_CHAR, encoding='utf-8')
 		# c.setdecoding(pyodbc.SQL_WCHAR, encoding='utf-8')
@@ -31,24 +31,27 @@ def query(host,user,pw):
 def write():
 	marqueeDir = os.path.dirname(os.path.abspath(__file__))
 	configPath = os.path.join(marqueeDir,'config','config.ini')
-	outputDir = os.path.join(marqueeDir,'output-files')
+	# outputDir = os.path.join(marqueeDir,'output-files')
 	print(configPath)
-	print(outputDir)	
+	# print(outputDir)	
 	config = configparser.SafeConfigParser()
 	config.read_file(open(configPath,'r'))
 	user = config['FileMaker']['user']
 	pw = config['FileMaker']['pass']
 	host = config['FileMaker']['host']
+	db = config['FileMaker']['db_name']
+	outputDir = config['other stuff']['output_dir']
 	# print(os.listdir(outputDir))
 	# print(query())
 	for textfile in os.listdir(outputDir):
 		textfilePath = os.path.join(outputDir,textfile)
-		if textfile.endswith(".txt"):
-			fileNumber = int(textfile[0])
+		if "BAMPFA Front" in textfile or "BAMPFA Side" in textfile:
+			base = os.path.splitext(textfile)[0]
+			fileNumber = int(base[-1])
 			fileIndex = fileNumber - 1
 			try: 
 				with open(textfilePath,'w+') as valuefile:
-					valuefile.write(query(host,user,pw)[fileIndex])
+					valuefile.write(query(host,db,user,pw)[fileIndex])
 			except:
 				print("something went wrong")
 				pass
